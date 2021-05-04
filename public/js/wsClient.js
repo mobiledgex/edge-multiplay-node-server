1ca9cf0a00f91223a70874a654c79bbc1333f77f
Ahmed Schrute <ahmedhussam.hellomobile@gmail.com>
2021-05-04 11:24:15 -0700

    fix wsClient and css imports

--- public/js/wsClient.js
+++ public/js/wsClient.js
@@ -1,4 +1,11 @@
-const wsClient = new WebSocket('ws://0.0.0.0:7776')
+const host = window.location.hostname
+const port = window.location.port
+const wsClient = new WebSocket('ws://' + host + ':' + port)
+wsClient.addEventListener('open', () => {
+  var liveFeed = document.getElementById('live-feed');
+  liveFeed.classList.add('live-feed')
+})
+//fixme add live-feed class attribute once the ws client is open
 wsClient.addEventListener('message', (event) => {
   var stats = JSON.parse(event.data)
   document.getElementById('numOfConnectedClients').innerHTML = stats.numOfConnectedClients;
@@ -9,5 +16,5 @@ wsClient.addEventListener('message', (event) => {
   document.getElementById('countOfPlayersFullRooms').innerHTML = stats.membersInFullRooms;
 })
 wsClient.addEventListener('close', () => {
-  document.getElementById("live-feed").removeAttribute("class");
+  document.getElementById('live-feed').removeAttribute('class');
 })
